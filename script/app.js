4b8053dd7c833c8224da7d2cd88d38374e980694
waldekglaz <waldekglaz@gmail.com>
2021-12-27 17:53:25 +0100

    custom tip input js, input outline

--- script/app.js
+++ script/app.js
@@ -4,43 +4,40 @@ const elNumberOfPeople = document.querySelector('#people');
 const elTipPerPerson = document.querySelector('.calculator__tip-amount-display');
 const elTotalPerPerson = document.querySelector('.calculator__total-amount-display');
 const elButton = document.querySelector('.btn');
-const elCustomTip = document.querySelector('.custom-tip')
+const elCustomTip = document.querySelector('.custom-tip');
+const elAlert = document.querySelector('.alert');
+
 let billValue = 0;
 let numberOfPeople = 0;
 let tipValue = 0;
 
 elBill.addEventListener('change', ()=>{
-    billValue = parseFloat(elBill.value);
+        billValue = parseFloat(elBill.value);
     return billValue;
+    
 })
 
 tipButtons.forEach(btn=>{
     btn.addEventListener('click', (e)=>{
         e.target.classList.add('active');
-        
         let tipAsString = (e.target.textContent).toString();
         tipValue = parseFloat(tipAsString) / 100;
         return tipValue;
     })
 })
-elCustomTip.addEventListener('click', ()=>{
-    if(elCustomTip.value === ""){
-        tipButtons.forEach(btn=>{
-            btn.addEventListener('click', (e)=>{
-                e.target.classList.add('active');
-                let tipAsString = (e.target.textContent).toString();
-                tipValue = parseFloat(tipAsString) / 100;
-                return tipValue;
-            })
-        })
-    }else{
-        tipValue = parseFloat(elCustomTip.value) / 100;
+elCustomTip.addEventListener('change',(e)=>{
+        tipValue = parseFloat(e.target.value) / 100;
         return tipValue;
-    }
-    console.log(elCustomTip.getAttribute('placeholder'))
 })
 elNumberOfPeople.addEventListener('change', ()=>{
     numberOfPeople = elNumberOfPeople.value;
+    if(numberOfPeople == 0){
+        elAlert.classList.add('active');
+        return;
+    }else{
+        elAlert.classList.remove('active')
+    }
+    
     let totalTip =  (billValue * tipValue) / numberOfPeople;
     
     elTipPerPerson.textContent = `$${totalTip}`;
