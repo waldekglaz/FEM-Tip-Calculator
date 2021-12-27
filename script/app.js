4cec83fef9447d1621b09d902a5076ec2c77f57d
waldekglaz <waldekglaz@gmail.com>
2021-12-27 17:16:22 +0100

    reset btn js

--- script/app.js
+++ script/app.js
@@ -4,25 +4,41 @@ const elNumberOfPeople = document.querySelector('#people');
 const elTipPerPerson = document.querySelector('.calculator__tip-amount-display');
 const elTotalPerPerson = document.querySelector('.calculator__total-amount-display');
 const elButton = document.querySelector('.btn');
+const elCustomTip = document.querySelector('.custom-tip')
 let billValue = 0;
 let numberOfPeople = 0;
 let tipValue = 0;
 
 elBill.addEventListener('change', ()=>{
     billValue = parseFloat(elBill.value);
-    console.log(typeof(billValue))
     return billValue;
 })
 
 tipButtons.forEach(btn=>{
     btn.addEventListener('click', (e)=>{
         e.target.classList.add('active');
+        
         let tipAsString = (e.target.textContent).toString();
         tipValue = parseFloat(tipAsString) / 100;
         return tipValue;
     })
 })
-
+elCustomTip.addEventListener('click', ()=>{
+    if(elCustomTip.value === ""){
+        tipButtons.forEach(btn=>{
+            btn.addEventListener('click', (e)=>{
+                e.target.classList.add('active');
+                let tipAsString = (e.target.textContent).toString();
+                tipValue = parseFloat(tipAsString) / 100;
+                return tipValue;
+            })
+        })
+    }else{
+        tipValue = parseFloat(elCustomTip.value) / 100;
+        return tipValue;
+    }
+    console.log(elCustomTip.getAttribute('placeholder'))
+})
 elNumberOfPeople.addEventListener('change', ()=>{
     numberOfPeople = elNumberOfPeople.value;
     let totalTip =  (billValue * tipValue) / numberOfPeople;
@@ -33,5 +49,8 @@ elNumberOfPeople.addEventListener('change', ()=>{
 function clearDisplay(){
     elTipPerPerson.textContent = "$0";
     elTotalPerPerson.textContent = "$0";
+    tipButtons.forEach(btn=>{
+        btn.classList.remove('active')
+    })
 }
 elButton.addEventListener('click', clearDisplay);
